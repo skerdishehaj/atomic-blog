24bc9c8e625041a0c3ee13a57bae467f9cc226aa
Skerdi Shehaj <skerdianshehaj@gmail.com>
2023-10-23 00:00:57 +0200

    Adding the SlowComponent as a children prop inside the Counter so the SlowComponent will be rendered independently of the count state changes

--- src/App-v1.js
+++ src/App-v1.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { faker } from '@faker-js/faker';
 import { PostProvider, usePosts } from './PostContext';
+import Test from './Test';
 
 // This function generates a random post object with a title and a body
 function createRandomPost() {
@@ -136,14 +137,17 @@ function FormAddPost() {
 function List() {
   const { posts } = usePosts();
   return (
-    <ul>
-      {posts?.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts?.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+      {/* <Test />  */}
+    </>
   );
 }
 
--- src/Test.js
+++ src/Test.js
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 function SlowComponent() {
   // If this is too slow on your maching, reduce the `length`
-  const words = Array.from({ length: 100_000 }, () => "WORD");
+  const words = Array.from({ length: 100_000 }, () => 'WORD');
   return (
     <ul>
       {words.map((word, i) => (
@@ -14,13 +14,30 @@ function SlowComponent() {
   );
 }
 
-export default function Test() {
+const Counter = ({ children }) => {
   const [count, setCount] = useState(0);
   return (
     <div>
       <h1>Slow counter?!?</h1>
       <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-      <SlowComponent />
+      {children}
+    </div>
+  );
+};
+
+/*
+ ! Optimizations techniques 
+ * 1) By setting the SlowComponent as a children prop of the Counter component, the SlowComponent the SlowComponent will be rendered independently from the Counter component. This means that the SlowComponent will not be re-rendered when the Counter component is re-rendered (every count state change).
+ */
+
+export default function Test() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <Counter>
+        <SlowComponent />
+      </Counter>
     </div>
   );
 }
+
